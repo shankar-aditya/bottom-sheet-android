5a22163e65855dc008ed7a08a8382b0a73fb9ac1
adishank <adishank@visa.com>
2022-11-12 18:47:52 +0530

    change dialog to rounded top

--- app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.bottomsheetdialog
 
-import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -18,15 +17,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.bottomsheetdialog.ui.theme.BottomSheetDialogTheme
 import kotlinx.coroutines.launch
-
 
 
 class MainActivity : ComponentActivity() {
@@ -42,7 +35,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BottomSheet(){
 
-    val reasonLists: List<String>? = listOf(
+    val reasonLists: List<String> = listOf(
         "Reason 1",
         "Reason 10",
         "Reason 11",
@@ -56,6 +49,7 @@ fun BottomSheet(){
     BottomSheetScaffold(
         sheetGesturesEnabled = false,
         scaffoldState = bottomSheetScaffoldState,
+        sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
         sheetContent = {
             Box(
                 Modifier
@@ -70,7 +64,7 @@ fun BottomSheet(){
                         text = "Title goes here.",
                         fontSize = 24.sp
                     )
-                    LazyColumn(){
+                    LazyColumn{
                         item {
                             Text(text = "Description goes here."
                                     )
